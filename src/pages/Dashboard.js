adb6d8860715c84fe436f8bf10f32fee1eb43dfe
Mohammed Arshad <mohdarshad102me@gmail.com>
2023-01-28 15:20:07 +0530

    Third commit

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,16 +1,12 @@
 import React from "react";
 import Sidebar from "../components/Sidebar";
 import Navbar from "../components/Navbar";
-import Table from "../components/BasicTable";
-import {data, columns} from "../components/BasicTableData";
-
 export default function Dashboard() {
   return (
     <>
       <Sidebar />
       <Navbar />
       <div className="Dashboard">
-      <Table data={data} columns={columns} />
       </div>
     </>
   );
